b7c184f8d346123850d07fd35c3308c5cb3173a7
Zennaliekens <zennaliekens@hotmail.com>
2024-06-10 13:47:04 +0200

    nieuwe mappen aangemaakt

--- visualise.py
+++ visualise.py
@@ -4,13 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def frequency_graph(list_of_n_moves) -> None:
-    #%matplotlib inline
-    #plt.rcParams.update({'figure.figsize':(7,5), 'figure.dpi':100})
+
+    # plt.rcParams.update({'figure.figsize':(7,5), 'figure.dpi':100})
     
     # Plot Histogram on x
-    plt.hist(list_of_n_moves, bins=1)
-    plt.gca().set(title='Frequency Histogram', ylabel='Frequency');
-    
+    plt.hist(list_of_n_moves, rwidth=1.0)
+    plt.xlabel('Amount of moves')
+    plt.ylabel('Frequency')
+    plt.title('Randomise table')
+    plt.show()
+
 
 if __name__ == "__main__":
     list1 = [1,1,4,5,2,2,4,4,4,7,9]
