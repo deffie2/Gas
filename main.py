478c8e77352a149fc99e994bbe984772bf40ec7a
CharlotteDasselaar <charlotte2002.05@gmail.com>
2024-06-11 12:03:41 +0200

    main change1.0

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from representatie import Board
 from randomise import move_car_random
 
-def main():
+#def main():
+
+
+if __name__ == "__main__":
+
+    # Call board
     board = Board(6)
     board.places_car()
     board.printboard()
+    
+    runs = input("How many times do you want to run the simulation?")
 
     # Check if the red car is already at the exit
-    if board.is_red_car_at_exit():
-        print("Red car is already at the exit! You won.")
-        return
-
-    for i in range(10):
-        print(f"Move {i + 1}")
-        move_car_random(board)
-        if board.is_red_car_at_exit():
-            print("Red car reached the exit! You won.")
-            break
-    else:
-        print("Red car did not reach the exit after 2 moves.")
-
-if __name__ == "__main__":
-    main()
+    #if board.is_red_car_at_exit():
+        #print("Red car is already at the exit! You won.")
+        #return
+        
+    moveslist = []
+    # All the runs
+    for i in runs:
+        moves = 0
+        # A single run
+        while not (board.is_red_car_at_exit()):
+            move_car_random(board)
+            moves += 1
+        print(f"It took {moves} moves")
+        moveslist.append(moves)
+    
+    
+    
+    #if board.is_red_car_at_exit():
+    #    print("Red car reached the exit! You won!")
+     #   print(f"It took{moves} moves")
+      #  break
+    
