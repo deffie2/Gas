b39518d80c697feda29ee4418eec83c1f03d16ba
CharlotteDasselaar <charlotte2002.05@gmail.com>
2024-06-11 16:52:21 +0200

    Hopen dat alles nu wel werkt, visualise

--- visualise.py
+++ visualise.py
@@ -1,20 +1,34 @@
-#
-
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
-def frequency_graph(list_of_n_moves) -> None:
+def frequency_graph(name_data: str, name_new_file: str) -> None:
+    """
+    Input: name of CSV file
+    Output: Plots a Histogram of moves for a certain board
+    """
 
-    # plt.rcParams.update({'figure.figsize':(7,5), 'figure.dpi':100})
+    list_of_n_moves = []
     
+    # Open file and save data in list
+    with open(f"{name_data}") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            for item in row:
+                item = int(item.lstrip('\ufeff'))
+                list_of_n_moves.append(item)
+            
     # Plot Histogram on x
     plt.hist(list_of_n_moves, rwidth=1.0)
     plt.xlabel('Amount of moves')
     plt.ylabel('Frequency')
     plt.title('Randomise table')
-    plt.show()
+    
+    # Save the plot to a file
+    plt.savefig(f'{name_new_file}.png')
 
 
 if __name__ == "__main__":
-    list1 = [1,1,4,5,2,2,4,4,4,7,9]
-    frequency_graph(list1)
+    name_data = str(input("What is the name of the dataset you wish to access? "))
+    name_new_file = str(input("How do you want to name your new png-file? "))
+    frequency_graph(name_data, name_new_file)
