1c854657afe7de8d9a32d122c02879d09ffa7e51
CharlotteDasselaar <charlotte2002.05@gmail.com>
2024-06-13 11:10:11 +0200

    Breadth First aanpassing

--- BreadthFirst.py
+++ BreadthFirst.py
@@ -2,4 +2,15 @@ import queue
 import copy
 
 def move_care_breadth_first():
-    depth = 3
+    depth = 50                   # No deeper than "depth"       
+    queue = queue.Queue()
+    queue.put("")               # Add begin state to queue
+    while not queue.empty():
+        state = queue.get()
+        print(state)
+        if len(state) < depth:
+            for i in ["L","R"]: # Wat is hier L en R? -> welke auto of hoeveel? 
+            # -> allebei leidt tot een beweging, dus beide nodig
+                child = copy.deepcopy(state) # deepcopy the state
+                child += i                   # make new child    
+                queue.put(child)             # add new child        
