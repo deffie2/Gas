919a3c5aa215ef9b11704b249c86fe1567fdf0dd
CharlotteDasselaar <charlotte2002.05@gmail.com>
2024-06-19 09:34:54 +0200

    Statesace calc

---
+++ code/visualisation/statespace.py
@@ -0,0 +1,9 @@
+# Berken statespace
+import math
+
+if __name__ == "__main__":
+    print(math.factorial(7) / (math.factorial(2)* math.factorial(5)))
+    print(math.factorial(6) / (math.factorial(2)* math.factorial(4)))
+    kolom = 21 * 8 * 8 * 7* 8 * 15 * 21 * 15
+    rij = 7 * 8 * 8 * 21 * 15 * 21 * 15 * 8
+    print(kolom * rij)
