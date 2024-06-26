89671cca9230dcbcb346fe3a3aae8b18bf01d67f
Deveney <d-etnel2@hotmail>
2024-06-26 22:58:40 +0200

    aanpassingen aan graphs_ex1.py

--- Experiment/graphs_for_ex1.py
+++ Experiment/graphs_for_ex1.py
@@ -16,30 +16,29 @@ for board_number in board_numbers:
     # Subset of data for the current board number and 'bs' algorithm
     df_board = df_bs[df_bs['Board'] == board_number]
     
-    # Grafiek 1: Tijd versus Beamwidth
-    plt.figure(figsize=(15, 5))  # Grootte van de figuur aanpassen indien nodig
-    plt.subplot(1, 3, 1)  # Eerste grafiek (1 rij, 3 kolommen, eerste positie)
-    plt.plot(df_board['Beam Width', df_board['Time']], marker='o')
+    # Grafiek 1: Time versus Beamwidth
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 3, 1)
+    plt.plot(df_board['Time'], df_board['Beam Width'], marker='o')
     plt.title(f'Board {board_number}: Time versus Beam Width')
-    plt.xlabel('Beam Width')
-    plt.ylabel('Time')
+    plt.xlabel('Time')
+    plt.ylabel('Beam Width')
 
     # Grafiek 2: State Space versus Beamwidth
-    plt.subplot(1, 3, 2)  # Tweede grafiek (1 rij, 3 kolommen, tweede positie)
-    plt.plot(df_board['Board states'], df_board['Beam Width'], marker='o')
+    plt.subplot(1, 3, 2)
+    plt.plot(df_board['Board States'], df_board['Beam Width'], marker='o')
     plt.title(f'Board {board_number}: State Space versus Beam Width')
     plt.xlabel('State Space')
     plt.ylabel('Beam Width')
 
     # Grafiek 3: Moves versus Beamwidth
-    plt.subplot(1, 3, 3)  # Derde grafiek (1 rij, 3 kolommen, derde positie)
+    plt.subplot(1, 3, 3)
     plt.plot(df_board['Moves'], df_board['Beam Width'], marker='o')
     plt.title(f'Board {board_number}: Moves versus Beam Width')
     plt.xlabel('Moves')
     plt.ylabel('Beam Width')
 
-    # Automatische uitlijning van de subplots
-    plt.tight_layout()
-
-    # Toon de grafieken voor dit boardnummer
+    # Determine where you want to save the graph.
+    save_path = f'./board_{board_number}'
+    plt.savefig(save_path)
     plt.show()
