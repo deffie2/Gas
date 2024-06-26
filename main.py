affd2eb601bbf26f6594b11bd76d9a434c129870
Deveney <d-etnel2@hotmail>
2024-06-26 23:07:43 +0200

    aanpassingen in de main.py

--- main.py
+++ main.py
@@ -12,24 +12,25 @@ from code.algorithms.beam_search import beam_search
 from code.visualisation.visualise import frequency_graph
 from code.visualisation.simulation import visualize
 
-# from visualise import frequency_graph
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 6:
+    # Check if the correct number of command-line arguments is provided.
+    # If not, print usage instructions and exit the script.
+    if len(sys.argv) < 5:
         print("Usage: script.py <game_number> <d> <algoritme> <runs> <shorest_path>")
         sys.exit(1)
     
+    # Parse command-line arguments
     game_number = int(sys.argv[1])
     d = int(sys.argv[2])
     algorithm = sys.argv[3]
     runs = int(sys.argv[4])
     shorest_path = sys.argv[5]
 
-    
 #     --------------------------- Random --------------------------------------
     if shorest_path == "no":
-
+        # Execute random algorithms based on command-line argument 'algorithm'
         if algorithm == "r" and shorest_path == "no":
             csv_names = move_car_random_WOH(d, game_number, runs)
         elif algorithm == "rh" and shorest_path == "no":
@@ -37,66 +38,44 @@ if __name__ == "__main__":
             csv_names[0]
 
     # --------------------------- Breadth First -------------------------------
-
-    # Breadth-first search without heuristics applied to game boards 1, 2, 3, and 4.
-
+    # Perform breadth-first search on specified game board and dimensions
     if algorithm == "bf" and shorest_path == "no":
         solution = breadth_first_search(d, game_number, runs)
         
 
     # --------------------------- Beam Search   -------------------------------
-
+    # Execute beam search algorithm on specified game board and dimensions
     if algorithm == "bs" and shorest_path == "no":
         solution = beam_search(d, game_number, runs)
 
     # --------------------------- Visualisation -------------------------------
-
-    # Graps
-
+    # Generate graphs or visualizations based on algorithm results
     if shorest_path == "no": 
 
         if algorithm == "r" or algorithm == "rh":
             frequency_graph(csv_names[0], csv_names[2])
             visualize(d, game_number, csv_names[1])
 
-        # Simulation
-
-        elif algorithm == "bf":
+        elif algorithm == "bf" or "bs":
             visualize(d, game_number, solution)
-
-        elif algorithm == "bs":
-            visualize(d, game_number, solution)
-
 
     # --------------------------------------------------------------------------
-    # shorest path only simulation
-
+    # Perform simulation for shortest path only
     if shorest_path == "yes":
-
-        if algorithm == "r":
+        if algorithm == "r" or "rh":
+            # Visualize shortest path for random with and withour heuristics
             name_data = str(input("What is the name of the dataset you wish to access? "))
             path = f'data/Random/Best_Moves_WOH/{name_data}'
             visualize(d, game_number, path)
 
-        elif algorithm == "rh":
-            name_data = str(input("What is the name of the dataset you wish to access? "))
-            path = f'data/Random/Best_Moves_WH/{name_data}'
-            visualize(d, game_number, path)
-
         elif algorithm == "bf":
+            # Visualize shortest path for breadth-first search
             path = f'data/Breadth_First/board_{game_number}_{d}X{d}.csv'
             visualize(d, game_number, path)
 
         elif algorithm == "bs":
+            # Visualize shortest path for beam search
             name_data = str(input("What is the name of the dataset you wish to access? "))
             path = f'data/Beam_Search/{name_data}'
             visualize(d, game_number, path)
-
-
-
-
-
-
-
-
     
